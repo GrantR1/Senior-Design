321e490c4544fc083c0a9269c66aa65c98129562
GrantR1 <greinecke0731@gmail.com>
2025-02-25 21:27:45 -0500

    Added some deck stuff

--- ScenesandScripts/deck.gd
+++ ScenesandScripts/deck.gd
@@ -15,8 +15,8 @@ func _ready() -> void:
 
 func draw_card():
 	print("Pizza")
-	var card_drawn = player_deck[0]
-	player_deck.erase(card_drawn)
+	var card_drawn_name = player_deck[0]
+	player_deck.erase(card_drawn_name)
 	
 	if player_deck.size() == 0:
 		$Area2D/CollisionShape2D.disabled = true
@@ -26,10 +26,11 @@ func draw_card():
 	
 	var card_scene = preload(CARD_SCENE_PATH)
 	var new_card = card_scene.instantiate()
-	#var card_image_path = str()
-	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn][0])
-	new_card.get_node("Def").text = str(card_database_reference.CARDS[card_drawn][1])
-	new_card.get_node("Cost").text = str(card_database_reference.CARDS[card_drawn][2])
+	var card_image_path = str("res://Sprites/" + card_drawn_name + ".png")
+	new_card.get_node("BackCard").texture = load(card_image_path)
+	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn_name][0])
+	new_card.get_node("Def").text = str(card_database_reference.CARDS[card_drawn_name][1])
+	new_card.get_node("Cost").text = str(card_database_reference.CARDS[card_drawn_name][2])
 	$"../Card Manager".add_child(new_card)
 	new_card.name = "Card"
 	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
