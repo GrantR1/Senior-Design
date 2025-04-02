6d056b3aa2954184e22a0ab73adb23fa58562795
GrantR1 <greinecke0731@gmail.com>
2025-04-01 20:02:47 -0400

    Changed some guardians to spells

--- ScenesandScripts/CardDatabase.gd
+++ ScenesandScripts/CardDatabase.gd
@@ -1,9 +1,9 @@
 const CARDS = { #attack #defense #cost, card type
-	"Teachers Assistant" : [2,1,1, "Guardian"], 
-	"The Professor" : [1,3,3, "Guardian"], 
-	"Moody Student" : [1,2,2, "Guardian"],
+	"Teachers Assistant" : [2,1,1, "Spell"], 
+	"The Professor" : [1,3,3, "Spell"], 
+	"Moody Student" : [1,2,2, "Spell"],
 	"SS Stevens victory": [0, 5,3, "Guardian"],
 	"Henry Morton": [1,2,2, "Spell"],
 	"University Center Complex": [0, 2, 1, "Guardian"],
-	"Sleep Deprived": [0,0,2, "Guardian"]
+	"Sleep Deprived": [0,0,2, "Spell"]
 }
