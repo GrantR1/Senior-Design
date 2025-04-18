5598b9f7a90473bfa7bb36cc80116a7585ae6ea6
Sean Maida <smaida2002@yahoo.com>
2025-04-18 11:34:19 -0400

    edited choose your deck

--- Choose A Deck/choose_your_deck.gd
+++ Choose A Deck/choose_your_deck.gd
@@ -32,25 +32,46 @@ func _on_exit_pressed() -> void:
 	
 func _on_Deck_One_Pressed() -> void:
 	deckInfoText.clear();
-	deckInfoText.add_text("Deck One Info");
+	deckInfoText.add_text("Student Deck\n
+	Allies: Undergrad, The Professor, Study Buddy, Atilla the Duck\n
+	Guardians: Stevens’ S.C. Williams Library, University Center Complex\n
+	Spells: Grading Curve, Extra Credit, Prepared to Learn, Failed Test, 
+	Honor Code Violation, Study Session");
 	DeckVars.your_deck = "deck one"
 	set_process(false);
 	
 func _on_Deck_Two_Pressed() -> void:
 	deckInfoText.clear();
-	deckInfoText.add_text("Deck Two Info");
+	deckInfoText.add_text("Statistics Deck\n
+	Allies: Teachers Assistant, The Professor, Moody Student\n
+	Guardians: S.S. Stevens, Pop Quiz, Stevens Campus Police\n
+	Spells: Extra Credit, Failed Test, Study Session, Tough Classes, 
+	MA-222 Probability and Statistics, MA-623 Stochastic Processes");
 	DeckVars.your_deck = "deck Two"
 	set_process(false);
 	
 func _on_Deck_Three_Pressed() -> void:
 	deckInfoText.clear();
-	deckInfoText.add_text("Deck Three Info");
+	deckInfoText.add_text("Mixed Deck\n
+	Allies: Study Buddy, Atilla the Duck, Residential Advisor\n
+	Guardians: S.S. Stevens, S.S Stevens Victory, Walker Gymnasium\n
+	Spells: TI-Nspire Graphing Calculator, Failed Test, A+ Test, 
+	Sleep Deprived, Tough Classes, Honor Code Violation");
 	DeckVars.your_deck = "deck three"
 	set_process(false);
 	
 func _on_Deck_Four_Pressed() -> void:
 	deckInfoText.clear();
-	deckInfoText.add_text("Deck Four Info");
+	deckInfoText.add_text("History Deck\n
+	Allies: Edwin A. Stevens, William H. Peirce, Henry Morton\n
+	Guardians: The Center for Maritime Systems at Stevens, 
+	Stevens' S.C. Williams Library, Walker Gymnasium\n
+	Spells: Charles V. Schaefer Jr. School of Engineering and Science, 
+	National Center for Secure and Resilient, 
+	Office of Innovation and Entrepreneurship, 
+	The Center for Innovation in Engineering and Science Education (CIESE), 
+	The Center for Environmental Systems (CES), 
+	The Highly Filled Materials Institute (HfMI)");
 	DeckVars.your_deck = "deck Four"
 	set_process(false);
 
